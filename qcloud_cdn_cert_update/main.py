2833d62643dcb434cb8d4ea555b719cbedb2bdde
terence chuen <tc@c4.hk>
2018-03-06 17:33:29 +0800

    1. 完成基本功能，待添加时间校验模块

--- qcloud_cdn_cert_update/main.py
+++ qcloud_cdn_cert_update/main.py
@@ -2,7 +2,6 @@
 
 import base64
 import json
-import os
 import re
 import sys
 
@@ -15,33 +14,32 @@ tmp_file_path = sys.path[0] + '/temp.log'
 cert_file_folder = sys.path[0] + '/cert/'
 
 
-def format_config_file():
-    if os.path.exists(config_file_path):
-        r = open(config_file_path, 'r')
-        c_json = json.load(r)
-        r.close()
-        return c_json
+def try_to_open_file(file_path):
+    try:
+        open_file = open(file_path, 'r')
+    except IOError as e:
+        print(e)
+        sys.exit(0)
     else:
-        return False
+        read_file = open_file.read()
+        open_file.close()
+        return read_file
 
 
-if format_config_file() is False:
-    print('file config.json config error, script exit!')
-    sys.exit(0)
-else:
-    config_json = format_config_file()
+def check_cert_info(cert_file_path):
+    output_dict = dict()
 
-
-def check_cert_subjectaltname(cert_file_path):
-    r = open(cert_file_path, 'r')
-    r_content = r.read().encode()
-    r.close()
+    crt_content = try_to_open_file(cert_file_path).encode()
     cert_type = OpenSSL.crypto.FILETYPE_PEM
-    i = OpenSSL.crypto.load_certificate(cert_type, r_content)
-    i_count = i.get_extension_count()
+    j = OpenSSL.crypto.load_certificate(cert_type, crt_content)
+
+    output_dict['crt_not_before'] = j.get_notBefore().decode()
+    output_dict['crt_not_after'] = j.get_notAfter().decode()
+
+    j_count = j.get_extension_count()
     dns_name = ''
-    for j in range(i_count):
-        k = i.get_extension(j)
+    for f in range(j_count):
+        k = j.get_extension(f)
         k_name = k.get_short_name()
         if k_name.decode() == 'subjectAltName':
             dns_name = k
@@ -49,54 +47,52 @@ def check_cert_subjectaltname(cert_file_path):
     dns_name_chardet = chardet.detect(dns_name)
     dns_name = dns_name.decode(dns_name_chardet['encoding'])
     dns_name = re.sub(r'[^\S]', '', dns_name).split('‚')
-    return dns_name
-
-
-def format_cert_key(i):
-    crt_file_path = cert_file_folder + i + '.crt'
-    key_file_path = cert_file_folder + i + '.key'
-    if os.path.exists(crt_file_path):
-        r = open(crt_file_path, 'r')
-        r_content = r.read()
-        r.close()
-        crt_base64 = base64.encodebytes(r_content.encode()).decode()
-    else:
-        print('cert error, script exit!')
-        sys.exit(0)
-    if os.path.exists(key_file_path):
-        r = open(key_file_path, 'r')
-        r_content = r.read()
-        r.close()
-        key_base64 = base64.encodebytes(r_content.encode()).decode()
-    else:
-        print('key error, script exit!')
-        sys.exit(0)
-    output_dict = dict()
-    output_dict['crt'] = crt_base64
-    output_dict['key'] = key_base64
+    output_dict['dns_name'] = dns_name
     return output_dict
 
 
-for key, value in config_json.items():
-    print(key)
-    print(value)
-    config = {
-        'secretId': value['secret_id'],
-        'secretKey': value['secret_key'],
-    }
-    crt_key_dict = format_cert_key(key)
-    action = value['action']
-    module = value['module']
-    params = {
-        'host': key,
-        'httpsType': value['https_type'],
-        'forceSwitch': value['https_force_switch'],
-        'http2': value['http2'],
-        'cert': crt_key_dict['crt'],
-        'privateKey': crt_key_dict['key']
-    }
-    service = QcloudApi(module, config)
-    print(service.generateUrl(action, params))
-    # qcloud_output = service.call(action, params).decode()
+def format_cert_key(domain_name):
+    crt_file_path = cert_file_folder + domain_name + '.crt'
+    key_file_path = cert_file_folder + domain_name + '.key'
 
-    # print(qcloud_output)
+    if domain_name in check_cert_info(crt_file_path)['dns_name']:
+        crt_content = try_to_open_file(crt_file_path)
+        crt_base64 = base64.encodebytes(crt_content.encode()).decode()
+
+        key_content = try_to_open_file(key_file_path)
+        key_base64 = base64.encodebytes(key_content.encode()).decode()
+
+        output_dict = dict()
+        output_dict['crt'] = crt_base64
+        output_dict['key'] = key_base64
+        return output_dict
+    else:
+        return False
+
+
+config_json = json.loads(try_to_open_file(config_file_path))
+
+for key, value in config_json.items():
+    crt_key_dict = format_cert_key(key)
+
+    if crt_key_dict is False:
+        continue
+    else:
+        config = {
+            'secretId': value['secret_id'],
+            'secretKey': value['secret_key'],
+        }
+        action = value['action']
+        module = value['module']
+        params = {
+            'host': key,
+            'httpsType': value['https_type'],
+            'forceSwitch': value['https_force_switch'],
+            'http2': value['http2'],
+            'cert': crt_key_dict['crt'],
+            'privateKey': crt_key_dict['key']
+        }
+        service = QcloudApi(module, config)
+        print(service.generateUrl(action, params))
+        # qcloud_output = service.call(action, params).decode()
+        # print(qcloud_output)
