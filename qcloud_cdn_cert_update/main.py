b7cdb8d0f1bd26c19cb8904c97f1b2e63adc51bb
TerenceChuen <contact@ngx.hk>
2018-09-30 14:03:56 +0800

    update

--- qcloud_cdn_cert_update/main.py
+++ qcloud_cdn_cert_update/main.py
@@ -2,15 +2,16 @@
 
 import base64
 import json
-import re
 import sys
 from datetime import datetime
-import OpenSSL
-import chardet
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+import re
+
 from QcloudApi.qcloudapi import QcloudApi
 
 # 配置文件、日志路径与证书文件夹
-config_file_path = sys.path[0] + '/config_private.json'
+config_file_path = sys.path[0] + '/config.json'
 tmp_file_path = sys.path[0] + '/temp.log'
 cert_file_folder = sys.path[0] + '/cert/'
 
@@ -31,37 +32,44 @@ def try_to_open_file(file_path):
 # 查找证书的有效期、生效时间
 # 查找证书中包含的所有域名
 def check_cert_info(cert_file_path):
-    output_dict = dict()
+    crt_open = open(cert_file_path, 'r')
+    crt_content = crt_open.read()
+    crt_open.close()
 
-    # 读取证书内容
-    crt_content = try_to_open_file(cert_file_path).encode()
-    # 定义证书格式
-    cert_type = OpenSSL.crypto.FILETYPE_PEM
-    # 加载证书
-    j = OpenSSL.crypto.load_certificate(cert_type, crt_content)
+    crt = x509.load_pem_x509_certificate(crt_content.encode(), default_backend())
 
-    # 获取证书生效时间，写入到字典以备输出
-    output_dict['crt_not_before'] = j.get_notBefore().decode()
-    # 获取证书失效时间，写入到字典以备输出
-    output_dict['crt_not_after'] = j.get_notAfter().decode()
+    # 获取alt name
+    crt_altname = crt.extensions.get_extension_for_oid(x509.oid.ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
+    crt_altname = crt_altname.value.get_values_for_type(x509.DNSName)
 
-    # 统计证书拓展字段数量
-    j_count = j.get_extension_count()
-    dns_name = ''
+    # 获取生效时间
+    crt_not_valid_before = crt.not_valid_before
+    # 获取过期时间
+    crt_not_valid_after = crt.not_valid_after
 
-    # 遍历拓展，寻找名为subjectAltName的字段
-    for f in range(j_count):
-        k = j.get_extension(f)
-        k_name = k.get_short_name()
-        if k_name.decode() == 'subjectAltName':
-            dns_name = k
-    # 从subjectAltName字段中提取域名，以列表保存并写入到字典以备输出
-    dns_name = dns_name.get_data()
-    dns_name_chardet = chardet.detect(dns_name)
-    dns_name = dns_name.decode(dns_name_chardet['encoding'])
-    dns_name = re.sub(r'[^\S]', '', dns_name).split('‚')
-    output_dict['dns_name'] = dns_name
-    return output_dict
+    crt_output = dict()
+    crt_output['crt_altname'] = crt_altname
+    crt_output['crt_not_valid_before'] = crt_not_valid_before
+    crt_output['crt_not_valid_after'] = crt_not_valid_after
+
+    return crt_output
+
+
+def crt_chk_alt_name(domain, alt_name):
+    for i in alt_name:
+        if i[0] == "*":
+            i = i[2:]
+            domain = domain.replace(i, '')
+            if domain.count('.') == 1:
+                return True
+            else:
+                pass
+        else:
+            if i == domain:
+                return True
+            else:
+                pass
+    return False
 
 
 # 检查证书是否已生效
@@ -72,20 +80,21 @@ def format_cert_key(domain_name, crt_file_name, key_file_name):
     key_file_path = cert_file_folder + key_file_name
 
     check_cert_info_dict = check_cert_info(crt_file_path)
+    chk_alt_name = crt_chk_alt_name('cdn.ngx.hk', check_cert_info_dict['crt_altname'])
 
     # 判断域名是否在该证书中
-    if domain_name in check_cert_info_dict['dns_name']:
+    if chk_alt_name:
         # 获取本地UTC时间
         datetime_now = datetime.utcnow()
 
         # 将字符串解析为时间元组
-        crt_not_before = datetime.strptime(check_cert_info_dict['crt_not_before'], '%Y%m%d%H%M%SZ')
-        crt_not_after = datetime.strptime(check_cert_info_dict['crt_not_after'], '%Y%m%d%H%M%SZ')
+        crt_not_valid_before = check_cert_info_dict['crt_not_valid_before']
+        crt_not_valid_after = check_cert_info_dict['crt_not_valid_after']
 
         # 如果现在的时间大于证书生效时间，则为True
-        if datetime_now >= crt_not_before:
+        if datetime_now >= crt_not_valid_before:
             # 如果证书失效时间-现在的时间大于配置文件中validity（单位：天）的值，则为True
-            if (crt_not_after - datetime_now).days >= int(config_json[domain_name]['validity']):
+            if (crt_not_valid_after - datetime_now).days >= int(config_json[domain_name]['validity']):
                 crt_content = try_to_open_file(crt_file_path)
                 crt_base64 = base64.encodebytes(crt_content.encode()).decode()
 
@@ -100,6 +109,8 @@ def format_cert_key(domain_name, crt_file_name, key_file_name):
                 return False
         else:
             return False
+    else:
+        return False
 
 
 # 载入配置文件
@@ -123,6 +134,7 @@ for key, value in config_json.items():
 
     # 调用函数，检查证书是否合规
     crt_key_dict = format_cert_key(key, cert_filename, key_filename)
+    # print(crt_key_dict)
 
     # 不合规则跳过，进入下一循环
     if crt_key_dict is False:
@@ -150,4 +162,4 @@ for key, value in config_json.items():
         # print(service.generateUrl(action, params))
         # 执行API
         qcloud_output = service.call(action, params).decode()
-        # print(qcloud_output)
+        print(qcloud_output)
