27d61da6f76aea95c7bd3c2cd80caec06bb8bdec
Terence Chuen <contact@ngx.hk>
2019-01-31 20:04:12 +0800

    调整逻辑，增加功能，待完成

--- qcloud_cdn_log_download/main.py
+++ qcloud_cdn_log_download/main.py
@@ -26,14 +26,18 @@ else:
     config_json = json.loads(config_content)
     open_config.close()
 
-# 读取临时文件
-try:
-    open_tmp_file = open(tmp_file_path, 'r')
-except IOError:
-    start_date = False
-else:
-    start_date = open_tmp_file.read()
-    open_tmp_file.close()
+
+def read_tmp_file(i_tmp_log_path):
+    # 读取临时文件
+    try:
+        open_tmp_file = open(tmp_file_path, 'r')
+    except IOError:
+        start_date = False
+    else:
+        start_date = open_tmp_file.read()
+        open_tmp_file.close()
+    return start_date
+
 
 datetime_now = datetime.datetime.now()
 date_now = datetime_now.strftime("%Y-%m-%d %H:%M:%S")
@@ -54,10 +58,10 @@ def process_output(i):
         return False, i['message']
 
 
-def download_log_file(i, j):
-    r = requests.get(j, stream=True)
+def download_log_file(i_zip_file_name, i_zip_file_link, i_zip_file_path):
+    r = requests.get(i_zip_file_link, stream=True)
     if r.status_code == 200:
-        with open(log_zip_file_path + i + '.gz', 'wb') as gz:
+        with open(i_zip_file_path + i_zip_file_name + '.gz', 'wb') as gz:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
                     gz.write(chunk)
@@ -67,23 +71,26 @@ def download_log_file(i, j):
         return r.status_code
 
 
-def uncompress_log_file(i):
-    log_gzip = gzip.open(log_zip_file_path + i + '.gz', 'rb')
-    log_text = open(log_unzip_file_path + i + '.log', 'wb')
+def uncompress_log_file(i_zip_file_name, i_zip_file_path, i_unzip_file_path):
+    log_gzip = gzip.open(i_zip_file_path + i_zip_file_name + '.gz', 'rb')
+    log_text = open(i_unzip_file_path + i_zip_file_name + '.log', 'wb')
     log_content = log_gzip.read()
     log_text.write(log_content)
     log_gzip.close()
     log_text.close()
 
 
-def write_log_file(i, j, k, l):
+def write_log_file(i_date_now, i_log_date, i_statue, i_message):
+    """
     script_log_dict = dict()
-    script_log_dict['date_now'] = i
-    script_log_dict['log_date'] = j
-    script_log_dict['statue'] = k
-    script_log_dict['message'] = l
+    script_log_dict['date_now'] = i_date_now
+    script_log_dict['log_date'] = i_log_date
+    script_log_dict['statue'] = i_statue
+    script_log_dict['message'] = i_message
+    """
     script_log_write = open(log_file_path, 'a+')
-    script_log_write.write('download_date:' + str(i) + ' log_name:' + str(j) + ' statue:' + k + ' message:' + l + '\n')
+    script_log_write.write('download_date:' + str(i_date_now) + ' log_name:' + str(
+        i_log_date) + ' statue:' + i_statue + ' message:' + i_message + '\n')
     script_log_write.close()
 
 
@@ -123,7 +130,7 @@ def download_log(qcloud_output):
         write_log_file(str(date_now), start_date, 'Failure', qcloud_output[1])
 
 
-if __name__ == '__main__':
+def run(i_start_date):
     cdn_config = {
         'Region': 'gz',
         'secretId': config_json['secret_id'],
@@ -131,7 +138,7 @@ if __name__ == '__main__':
         'method': 'get'
     }
 
-    if start_date is False:
+    if i_start_date is False:
         cdn_params = {
             'host': config_json['host_name']
         }
@@ -139,14 +146,14 @@ if __name__ == '__main__':
         download_log(cdn_log_download_link)
         write_tmp_log(date_now)
     else:
-        last_log_time = datetime.datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S')
+        last_log_time = datetime.datetime.strptime(i_start_date, '%Y-%m-%d %H:%M:%S')
         time_calculate = (datetime_now - last_log_time).seconds
         if time_calculate > 4500:
-            start_date = datetime.datetime.strftime(last_log_time, '%Y-%m-%d %H:00:00')
+            i_start_date = datetime.datetime.strftime(last_log_time, '%Y-%m-%d %H:00:00')
             end_date = datetime_now.strftime("%Y-%m-%d %H:00:00")
             cdn_params = {
                 'host': config_json['host_name'],
-                'startDate': str(start_date),
+                'startDate': str(i_start_date),
                 'endDate': str(end_date)
             }
             cdn_log_download_link = get_download_link(cdn_params, cdn_config)
@@ -154,3 +161,7 @@ if __name__ == '__main__':
             write_tmp_log(date_now)
         else:
             pass
+
+
+if __name__ == '__main__':
+    run(start_date)
